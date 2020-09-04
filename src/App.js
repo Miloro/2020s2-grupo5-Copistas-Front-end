34d532246751e1232847a04958de8080ce2df7ec
Miloro <miloromiguel@gmail.com>
2020-09-04 14:38:15 -0300

    Se subio un nivel de carpeta al  proyecto

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <h1>
+          Copistas
+        </h1>
+
+      </body>
+
+    </div>
+  );
+}
+
+export default App;
