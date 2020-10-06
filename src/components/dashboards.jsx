44eb3d2ed2ba8cfe00c99c4ebf706344da7803e5
Miloro <miloromiguel@gmail.com>
2020-10-06 12:27:41 -0300

    se puede mostrar algo de data en los graficos de torta

--- src/components/dashboards.jsx
+++ src/components/dashboards.jsx
@@ -1,35 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useLayoutEffect, Fragment } from "react";
 import NavBar from "./NavBar";
 import { Pie } from "react-chartjs-2";
+import { getDashboard } from "./Api";
 
 export default function Dashboards() {
-  const [genero, setGenero] = useState({
-    labels: ["Mujeres", "Hombres"],
+  const [dashBoards, setDashBoards] = useState();
+
+  useLayoutEffect(() => {
+    getDashboard().then((data) => {
+      setDashBoards(data);
+    });
+  }, [dashBoards]);
+
+  /*
+
+  */
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div class="d-flex flex-row bd-highlight mb-3">
+        {!!dashBoards ? <GraficosDeTorta data={dashBoards} /> : "cargando"}
+      </div>
+    </div>
+  );
+}
+
+function GraficosDeTorta({ data }) {
+  return (
+    <Fragment>
+      <GraficoDeTorta
+        elementos={["Mujeres", "Hombres"]}
+        cantidades={[data.dashboardSexoDTO.mujer, data.dashboardSexoDTO.hombre]}
+        class="w-25 p-3"
+      ></GraficoDeTorta>
+
+      <GraficoDeTorta
+        elementos={["Total", "Parcial"]}
+        cantidades={[
+          data.dashboardNivelCegueraDTO.total,
+          data.dashboardNivelCegueraDTO.parcial,
+        ]}
+        class="w-25 p-3"
+      ></GraficoDeTorta>
+    </Fragment>
+  );
+}
+
+function GraficoDeTorta({ elementos, cantidades, ...props }) {
+  const [data] = useState({
+    labels: elementos,
     datasets: [
       {
-        data: [10, 5],
-        backgroundColor: ["#FF6384", "#36A2EB"],
+        data: cantidades,
+        backgroundColor: [
+          "#FF6384",
+          "#36A2EB",
+          "#36AAAA",
+          "#AAA2EB",
+          "#FF8884",
+          "#333384",
+          "#123456",
+          "#AB4235",
+          "#161616",
+          "#442461",
+        ],
       },
     ],
   });
 
   return (
-    <div>
-      <NavBar></NavBar>
-      <div class="d-flex flex-row bd-highlight mb-3">
-        <div class="w-25 p-3">
-          <Pie data={genero} />
-        </div>
-        <div class="w-25 p-3">
-          <Pie data={genero} />
-        </div>
-        <div class="w-25 p-3">
-          <Pie data={genero} />
-        </div>
-        <div class="w-25 p-3">
-          <Pie data={genero} />
-        </div>
-      </div>
+    <div {...props}>
+      <Pie data={data} />
     </div>
   );
 }
