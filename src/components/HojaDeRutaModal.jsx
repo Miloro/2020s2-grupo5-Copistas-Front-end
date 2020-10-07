dcb6c6702df8b9b958c37be0207d1f5718958a3d
Miloro <miloromiguel@gmail.com>
2020-10-07 12:47:43 -0300

    refactor hoja de ruta modal

--- src/components/HojaDeRutaModal.jsx
+++ src/components/HojaDeRutaModal.jsx
@@ -1,38 +1,43 @@
-import React, {useEffect, useState} from 'react';
-import {Modal, ModalBody, ModalFooter, Button} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import {getHojaDeRuta} from './Api'
+import React, { useEffect, useState } from "react";
+import { Modal, ModalBody, ModalFooter, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { getHojaDeRuta } from "./Api";
 
+export default function HojaDeRutaModal({ libro, abierto, cerrarModal }) {
+  const [hojaDeRuta, setHojaDeRuta] = useState(null);
 
-export default function HojaDeRutaModal({libro, abierto, cerrarModal}) {
-
-    const [hojaDeRuta, setHojaDeRuta] = useState(null);
-
-    useEffect(() => {
-        getHojaDeRuta(libro.id).then((unaHojaDeRuta) => {
-            setHojaDeRuta(unaHojaDeRuta);
-        })
+  useEffect(() => {
+    getHojaDeRuta(libro.id).then((unaHojaDeRuta) => {
+      setHojaDeRuta(unaHojaDeRuta);
     });
+  });
 
-    function mostrarIteraciones() {
-        return hojaDeRuta.historialDeEstado.map(iteracion => {
-            return <div>{iteracion.tareaAsignada}</div>
-        })
+  function mostrarIteraciones() {
+    return hojaDeRuta.historialDeEstado.map((iteracion) => {
+      return <div>{iteracion.tareaAsignada}</div>;
+    });
+  }
 
-    }
-
+  function mostrarData() {
     return (
-        <Modal show={abierto}>
-            <ModalBody>
-                Historial de los estados del libro : {!!hojaDeRuta ? <div>{
-                mostrarIteraciones()
-            }</div> : "buscando..."}
-            </ModalBody>
-            <ModalFooter>
-                <Button variant="secondary" onClick={cerrarModal}>
-                    Cerrar
-                </Button>
-            </ModalFooter>
-        </Modal>
+      <div>
+        {hojaDeRuta.solicitante.nombre}
+        {mostrarIteraciones()}
+      </div>
     );
+  }
+
+  return (
+    <Modal show={abierto}>
+      <ModalBody>
+        Historial de los estados del libro :{" "}
+        {!!hojaDeRuta ? <div>{mostrarData()}</div> : "buscando..."}
+      </ModalBody>
+      <ModalFooter>
+        <Button variant="secondary" onClick={cerrarModal}>
+          Cerrar
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 }
