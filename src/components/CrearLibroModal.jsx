9769c977bca82e17148b17f85bf1a5c43fd43e18
ferblanco75 <ferblanco@gmail.com>
2020-10-30 17:47:30 -0300

    modifico layout del formulario crear libro mas similar a crear cliente

--- src/components/CrearLibroModal.jsx
+++ src/components/CrearLibroModal.jsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import {Modal} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import {crearLibro} from './Api'
 import Button from 'react-bootstrap/Button';
+import { Form,  Col, Modal} from "react-bootstrap";
 
 //export default function CrearLibroModal(props) {
 export default function CrearLibroModal({show, cerrarLibroModal, actualizarLibro, libroId}) {
@@ -15,6 +15,7 @@ export default function CrearLibroModal({show, cerrarLibroModal, actualizarLibro
         "idioma": "",
         "categoria": ""
     })
+
 
     const handleInputChange = (event) => {
         setLibro({
@@ -38,29 +39,50 @@ export default function CrearLibroModal({show, cerrarLibroModal, actualizarLibro
                 <Modal.Title>Crear Libro</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <form className="row">
-                    <label>Titulo</label>
-                    <input type="text" placeholder="Titulo" className="form-control" onChange={handleInputChange}
-                           name="titulo"/>
-                    <label>Nombre del autor</label>
-                    <input type="text" placeholder="Nombre del autor" className="form-control"
-                           onChange={handleInputChange} name="nombreAutor"/>
-                    <label>Apellido del autor</label>
-                    <input type="text" placeholder="Apellido del autor" className="form-control"
+            <Form noValidate>
+                <Form.Row xs={1} md={4}>
+                        <Col>
+                            <label>Titulo</label>
+                            <input type="text" placeholder="Titulo" className="form-control" onChange={handleInputChange}
+                             name="titulo"/>
+                        </Col>
+                        <Col>
+                            <label>Nombre del autor</label>
+                            <input type="text" placeholder="Nombre del autor" className="form-control"
+                            onChange={handleInputChange} name="nombreAutor"/>
+                        </Col>
+                        <Col>
+                            <label>Apellido del autor</label>
+                            <input type="text" placeholder="Apellido del autor" className="form-control"
                            onChange={handleInputChange} name="apellidoAutor"/>
-                    <label>Editorial</label>
-                    <input type="text" placeholder="Editorial" className="form-control" onChange={handleInputChange}
-                           name="editorial"/>
-                    <label>Edicion</label>
-                    <input type="text" placeholder="Edicion" className="form-control" onChange={handleInputChange}
+                        </Col>
+                 </Form.Row>
+                <Form.Row xs={1} md={4}>
+                             <Col>                             
+                                <label>Editorial</label>
+                                 <input type="text" placeholder="Editorial" className="form-control" onChange={handleInputChange}
+                                 name="editorial"/>
+                            </Col>
+                </Form.Row> 
+                 <Form.Row>
+                             <label>Edicion</label>
+                             <input type="text" placeholder="Edicion" className="form-control" onChange={handleInputChange}
                            name="edicion"/>
-                    <label>Idioma</label>
-                    <input type="text" placeholder="Idioma" className="form-control" onChange={handleInputChange}
-                           name="idioma"/>
-                    <label>Categoria</label>
-                    <input type="text" placeholder="Categoria" className="form-control" onChange={handleInputChange}
-                           name="categoria"/>
-                </form>
+                </Form.Row>
+                <Form.Row>
+                             <Col>
+                                <label>Idioma</label>
+                                <input type="text" placeholder="Idioma" className="form-control" onChange={handleInputChange}
+                                 name="idioma"/>
+                            </Col>
+                             <Col>
+                                <label>Categoria</label>
+                                <input type="text" placeholder="Categoria" className="form-control" onChange={handleInputChange}
+                                name="categoria"/>
+                             </Col>
+                </Form.Row>
+                
+                </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={cerrarLibroModal}>
