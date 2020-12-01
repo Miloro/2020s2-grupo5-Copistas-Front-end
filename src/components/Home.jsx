91de595925128ba4522fd0117902f86850881ce9
Miloro <miloromiguel@gmail.com>
2020-12-01 14:09:42 -0300

    Si te logueas como colaborador , que muestre un mensaje de bienvenida

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,30 @@
 import React, { Fragment } from "react";
-
+import UsuarioContext from './UsuarioContext';
+import Container from "react-bootstrap/Container";
 
 import NavBar from "./NavBar";
 
 export default function Home () {
 
     return (
-      <Fragment>
-        <NavBar />
+      <UsuarioContext.Consumer>{
+        context =>  {
+          console.log(context.usuario.nombre)
+          return(
+          <>
+            <Fragment>
+              <NavBar />
+              <Container id="contenedor">
+                {context.usuario.nombre?
+                <h1>Bienvenido a Libraille {context.usuario.nombre}</h1>:
+                null}
+              </Container>
+            </Fragment>
+          </>         
+          )
+        }
+    }
+    </UsuarioContext.Consumer>
 
-      </Fragment>
     );
 }
